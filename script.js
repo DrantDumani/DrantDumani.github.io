f4b2cc8dd54becc029cef7dd966bcb249c015cd9
DrantDumani <darnjames@gmail.com>
2019-11-25 19:01:58 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let projects = [
+	"Guardian_Artwork.png",
+	"guardian_screenshot.jpg",
+	"guardian_nendo.jpg",
+	"guardian_amiibo.jpg",
+	"Link.jpg"
+]
+
+//let current_Proj = projects
+let curr = 0
+var slides = document.getElementById("slides")
+slides.setAttribute("src", projects[curr])
+document.getElementById("change").onclick = function() {
+	if (curr + 1 < projects.length)
+	{
+		curr+=1
+		slides.setAttribute("src", projects[curr])
+	}
+	else
+	{
+		curr = 0
+		slides.setAttribute("src", projects[curr])
+	}
+}
